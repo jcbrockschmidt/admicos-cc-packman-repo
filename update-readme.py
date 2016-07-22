ba74d938d8bd4f91701d54b0d7e1e5436689120f
Ecmel B. CANLIER <ecbercnlmc@gmail.com>
2016-07-22 22:47:01 +0300

    Automate README.md generation from the repo.

---
+++ update-readme.py
@@ -0,0 +1,46 @@
+"""Updates README.md file of packman repos to contain all packages (and versions)"""
+
+from __future__ import print_function
+import re
+
+repoFile = "repo"
+readme_header = """# Admicos's packman repository
+My repo for [lyqyd/cc-packman](https://github.com/lyqyd/cc-packman)
+
+## Currently Includes:
+"""
+readme_footer = """Also, the [LICENSE](/LICENSE) file applies to all the files in this repo, unless specified otherwise.
+"""
+
+
+def main():
+	"""Main function"""
+
+	print("Opening repository file")
+	with open(repoFile, "r") as f: # Opens repo file
+		repoContents = f.read()
+
+	print("Compiling regexes")
+	packages = re.compile(r"(([^file]|^)name?\s=?\s.*)") # Gets all package names.
+	versions = re.findall(re.compile(r"(version?\s=?\s.*)"), repoContents) # Gets all package versions.
+	frmlinks = re.findall(re.compile(r"(--link?\s=?\s.*)"), repoContents) # Gets package links.
+
+	print("Parsing repository file")
+	cnt = 0
+	readme_packages = ""
+	for package in re.findall(packages, repoContents): # This was worse before, trust me.
+		readme_packages = readme_packages + "* [" + package[0].split("=")[1].strip() + "](" + frmlinks[cnt].split("=")[1].strip() + ") " + versions[cnt].split("=")[1].strip() + "\n"
+		cnt += 1
+	cnt = None
+
+	print("Saving")
+	with open("README.md", "w") as f: # Opens repo file
+		f.write(readme_header)
+		f.write(readme_packages + "\n")
+		f.write(readme_footer)
+
+	print("Done!")
+	return 0
+
+if __name__ == '__main__':
+	exit(main())
