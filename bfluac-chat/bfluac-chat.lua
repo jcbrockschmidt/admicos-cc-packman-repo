91c59f5614ce29130e03ad5c01ee4d7fa5010d23
Ecmel B. CANLIER <ecbercnlmc@gmail.com>
2016-07-23 18:10:18 +0300

    Automatic push.

--- bfluac-chat/bfluac-chat.lua
+++ bfluac-chat/bfluac-chat.lua
@@ -7,82 +7,81 @@ local admin = "Admicos"
 local outBuf = ""
 
 local function split(str, splitter)
-  local t = {}
-  local function helper(line) table.insert(t, line) return "" end
+    local t = {}
+    local function helper(line) table.insert(t, line) return "" end
     helper((str:gsub("(.-)" .. splitter, helper)))
-  return t
+    return t
 end
 
 local function runbf(bfcode, sender)
-  local bffile = fs.open(".bf-" .. sender, "w")
+    local bffile = fs.open(".bf-" .. sender, "w")
     bffile.write(bfcode)
-  bffile.close()
+    bffile.close()
 
-  oWR = write
-  oPR = print
+    oWR = write
+    oPR = print
 
-  shell.run("bfluac .bf-" .. sender .. " -printMem -yieldMore")
+    shell.run("bfluac .bf-" .. sender .. " -printMem -yieldMore")
 
-  write = function(a) outBuf = outBuf .. a end
-  print = function(a) chatBox.say("§6" .. sender .. "> §f" .. outBuf) outBuf = a end
+    write = function(a) outBuf = outBuf .. a end
+    print = function(a) chatBox.say("§6" .. sender .. "> §f" .. outBuf) outBuf = a end
 
-  chatBox.say("----RUNNING----")
-  local ok, err = pcall(function()
-    loadfile(".bf-" .. sender .. ".bf.lua")[1]()
-  end)
+    chatBox.say("----RUNNING----")
+    local ok, err = pcall(function()
+        local func, _ loadfile(".bf-" .. sender .. ".bf.lua")
+        func()
+    end)
 
-  chatBox.say(err)
+    if not ok then
+        chatBox.say("§6" .. sender .. "> §c" .. err)
+    else
+        chatBox.say("§6" .. sender .. "> §f" .. outBuf)
+        outBuf = ""
+    end
 
-  if not ok then
-    chatBox.say("§6" .. sender .. "> §c" .. err)
-  end
+    write = oWR
+    print = oPR
 
-  write = oWR
-  print = oPR
-
-  fs.delete(".bf-" .. sender .. ".bf.lua")
-  fs.delete(".bf-" .. sender)
-
-  chatBox.say("§6" .. sender .. "> §f" .. outBuf)
-  outBuf = ""
+    fs.delete(".bf-" .. sender .. ".bf.lua")
+    fs.delete(".bf-" .. sender)
 
 end
 
 local function main()
-  chatBox.setName("bfluac - Admicos")
-  chatBox.say("bfluac chat edition started, have fun!")
-  chatBox.say("\"__bf\" for more info")
+    chatBox.setName("bfluac - Admicos")
+    chatBox.say("bfluac chat edition started, have fun!")
+    chatBox.say("\"__bf\" for more info")
 
-  local log = fs.open("bfchat.log", "w")
-  while running do
-    chatEvent = {os.pullEvent("chat_message")}
+    local log = fs.open("bfchat.log", "w")
+    while running do
+        chatEvent = {os.pullEvent("chat_message")}
 
-    carg = split(chatEvent[4], " ")   --Command Args
-    cprx = table.remove(carg, 1) --Chat prefix
-    ccmd = table.remove(carg, 1) --Command
+        carg = split(chatEvent[4], " ")   --Command Args
+        cprx = table.remove(carg, 1) --Chat prefix
+        ccmd = table.remove(carg, 1) --Command
 
-    if cprx == "__bf" then
-      if carg == nil or ccmd == nil then
-        log.writeLine("[" .. chatEvent[3] .. ":help]")
-          chatBox.say("__bf <run> <bf code> --WIP")
-      else
-        log.writeLine("[" .. chatEvent[3] .. ":" .. ccmd .. "] " .. (unpack(carg) or ""))
-        if ccmd == "stop" then
-          if chatEvent[3] == admin then
-            running = false
-          else
-            chatBox.say("Hey! You're not " .. admin .. "!")
-          end
-        elseif ccmd == "run" then
-          runbf(carg[1], chatEvent[3])
-        else
-          chatBox.say(ccmd .. " is not a bfluac command")
+        if cprx == "__bf" then
+            if carg == nil or ccmd == nil then
+                log.writeLine("[" .. chatEvent[3] .. ":help]")
+                chatBox.say("__bf <run> <bf code> --WIP")
+            else
+                log.writeLine("[" .. chatEvent[3] .. ":" .. ccmd .. "] " .. (unpack(carg) or ""))
+                if ccmd == "stop" then
+                    if chatEvent[3] == admin then
+                        running = false
+                    else
+                        chatBox.say("Hey! You're not " .. admin .. "!")
+                    end
+                elseif ccmd == "run" then
+                    runbf(carg[1], chatEvent[3])
+                else
+                    chatBox.say(ccmd .. " is not a bfluac command")
+                end
+            end
         end
-      end
-    end
 
-   -- print(chatEvent[3] .. "> " .. chatEvent[4])
-  end
-  log.close()
-  chatBox.say("Goodbye!")
-end main()
+        -- print(chatEvent[3] .. "> " .. chatEvent[4])
+    end
+    log.close()
+    chatBox.say("Goodbye!")
+    end main()
