e73656b0dde4be0f3072104bcf92680758009e0c
Ecmel B. CANLIER <ecbercnlmc@gmail.com>
2016-09-09 11:40:27 +0000

    fix flake8 complaining on update-readme.py

--- update-readme.py
+++ update-readme.py
@@ -1,4 +1,4 @@
-"""Updates README.md file of packman repos to contain all packages (and versions)"""
+"""Updates README.md file of packman repos to contain packages and versions"""
 
 from __future__ import print_function
 import re
@@ -9,51 +9,64 @@ My repo for [lyqyd/cc-packman](https://github.com/lyqyd/cc-packman)
 
 ## Currently Includes:
 """
-readme_footer = """Also, the [LICENSE](/LICENSE) file applies to all the files in this repo, unless specified otherwise.
-"""
+readme_footer = "Also, the [LICENSE](/LICENSE) file applies to all the files" \
+    "in this repo unless specified otherwise."
 
 
 def main():
-	"""Main function"""
+    """Main function"""
 
-	print("Opening repository file")
-	with open(repoFile, "r") as f: # Opens repo file
-		repoContents = f.read()
+    print("Opening repository file")
+    with open(repoFile, "r") as f:  # Opens repo file
+        repoContents = f.read()
 
-	print("Compiling regexes")
-	packages = re.compile(r"(([^file]|^)name?\s=?\s.*)") # Gets all package names.
+    print("Compiling regexes")
 
-	versions = re.findall(re.compile(r"(version?\s=?\s.*)"), repoContents) # Gets all package versions.
-	frmlinks = re.findall(re.compile(r"(--link?\s=?\s.*)"), repoContents) # Gets package links.
+    # Gets all package names.
+    packages = re.compile(r"(([^file]|^)name?\s=?\s.*)")
 
-	print("Parsing repository file")
-	cnt = 0
-	readme_packages = ""
-	for package in re.findall(packages, repoContents): # This was worse before, trust me.
-		try:
-			readme_packages = readme_packages + "* [" + package[0].split("=")[1].strip() + "](" + frmlinks[cnt].split("=")[1].strip() + ") " + versions[cnt].split("=")[1].strip() + "\n"
-			cnt += 1
-		except IndexError as ie:
-			print("""
+    # Gets all package versions.
+    versions = re.findall(re.compile(r"(version?\s=?\s.*)"), repoContents)
+
+    # Gets package links.
+    frmlinks = re.findall(re.compile(r"(--link?\s=?\s.*)"), repoContents)
+
+    print("Parsing repository file")
+    cnt = 0
+    readme_packages = ""
+
+    # This was worse before, trust me.
+    for package in re.findall(packages, repoContents):
+        try:
+            readme_packages = "%s* [%s](%s) %s\n" % (
+                readme_packages,
+                package[0].split("=")[1].strip(),
+                frmlinks[cnt].split("=")[1].strip(),
+                versions[cnt].split("=")[1].strip()
+            )
+
+            cnt += 1
+        except IndexError as ie:
+            print("""
 You either forgot to version your package, or forgot a link for it.
 You can add a link by creating a \"--link\" key before \"end\"
 
 Example:
-	dependencies = none
-	--link = https://computercraft.info/forums2/MYCOOLPACKAGE-thread
+    dependencies = none
+    --link = https://computercraft.info/forums2/MYCOOLPACKAGE-thread
 
 IndexError: """ + str(ie))
-			return 1
-	cnt = None
+            return 1
+    cnt = None
 
-	print("Saving")
-	with open("README.md", "w") as f: # Opens repo file
-		f.write(readme_header)
-		f.write(readme_packages + "\n")
-		f.write(readme_footer)
+    print("Saving")
+    with open("README.md", "w") as f:  # Opens repo file
+        f.write(readme_header)
+        f.write(readme_packages + "\n")
+        f.write(readme_footer)
 
-	print("Done!")
-	return 0
+    print("Done!")
+    return 0
 
 if __name__ == '__main__':
-	exit(main())
+    exit(main())
