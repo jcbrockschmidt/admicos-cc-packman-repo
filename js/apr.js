8a728574827b04dd56bbf96ceeda4f87690d9010
Ecmel B. CANLIER <ecbercnlmc@gmail.com>
2016-09-26 20:27:46 +0300

    list packages

--- js/apr.js
+++ js/apr.js
@@ -1,5 +1,34 @@
 $(document).ready(function() {
+    var pkg_template = `
+    <div class="col-md-6">
+        <div class="panel panel-default">
+            <div class="panel-heading"><a href="{5}"><h2 class="panel-title">{0}</h2></a></div>
+            <div class="panel-body">
+                <b>Categories:      </b>{1}<br>
+                <b>Version:         </b>{2}<br>
+                <b>Size:            </b>{3}<br>
+                <b>Dependencies:    </b>{4}<br>
+            </div>
+        </div>
+    </div>`;
+
     var repo_url = "https://raw.githubusercontent.com/Admicos/cc-packman-repo/master/repo";
     var loader   = $("#loading");
+    var pkgs     = $("#packages");
     var repofile = parseRepoFile(repo_url);
+
+    var pkg_html = "";
+
+    for(var thing in repofile) {
+        var pkg = repofile[thing];
+
+        pkg_html += pkg_template.format(pkg["name"], pkg["category"].replace(" ", ", "),
+                                        pkg["version"], pkg["size"],
+                                        pkg["dependencies"], pkg["--link"]);
+    }
+
+    loader.css({"display": "none"});
+    pkgs.html(pkg_html);
+
+    alert(pkg_html);
 });
--- js/packman.js
+++ js/packman.js
@@ -1,6 +1,40 @@
+$.ajaxSetup({async: false});
+
+/* Went better than expected. */
+function actuallyParseItForReal(repofile)
+{
+    var rlines = repofile.split("\n");
+    var out = {}
+    var cPKG = "";
+
+    for(var ln in rlines) {
+        var thing = rlines[ln].split("=");
+
+        switch (thing[0].trim()) {
+            case "name":
+                cPKG = thing[1].trim();
+                out[cPKG] = {"name": cPKG};
+            case "end":
+                break;
+            default:
+                if (out[cPKG] == null) {
+                    console.error("Y u gotta have no name?");
+                    break;
+                }
+
+                out[cPKG][thing.shift().trim()] = thing.join("=").trim();
+        }
+    }
+
+    return out;
+}
+
 function parseRepoFile(fileURL)
 {
+    var _data = null;
     $.get(fileURL, function(data) {
-        alert(data);
+        _data = actuallyParseItForReal(data);
     });
+
+    return _data;
 }
